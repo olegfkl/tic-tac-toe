24147b32a09a7bff8b4b1ef43a817d8424f48fec
Oleg Grushetskyy <olegfkl@gmail.com>
2018-01-28 13:23:25 +0000

    First commit

---
+++ spec/GameSpec.js
@@ -0,0 +1,12 @@
+describe("Game", function(){
+  var game;
+
+  it("should create an instance of a game", function(){
+    game = new Game();
+    expect(game).toEqual(jasmine.any(Game))
+  })
+
+  it("", function() {
+
+  })
+})
